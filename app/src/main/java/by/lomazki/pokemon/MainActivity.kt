552b838c78ad5e2f2928788fdd98df3c33f3804d
Lomazki <sedovlasovich@yandex.by>
2022-08-31 09:04:09 +0300

    New project

---
+++ app/src/main/java/by/lomazki/pokemon/MainActivity.kt
@@ -0,0 +1,11 @@
+package by.lomazki.pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
